ecf01b69f733f9c0935c1fc19b81a1849699eaf2
chrismgonzalez <cmgonza89@gmail.com>
2019-02-10 22:12:05 -0600

    added headers to student.cpp

--- student_data/student.cpp
+++ student_data/student.cpp
@@ -1,4 +1,7 @@
-#include "pch.h"
+#pragma once
+#include <iostream>
+#include <string>
+#include "degree.h"
 #include "student.h"
 
 Student::Student(string studentId, string firstName, stringLastName, string email, int age, int * daysToCompleteCourse, Degree::DegreeType degree)
